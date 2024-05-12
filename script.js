7be886d1592eec2bd6711bf7096b35de33650c12
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-05-12 16:48:45 +0800

    Activate accordion button

--- script.js
+++ script.js
@@ -7,3 +7,21 @@
 // smallIcon.src = './img/odin-logo.svg';
 
 // body.appendChild(smallIcon);
+
+(function activateButtons() {
+  const buttons = Array.from(document.querySelectorAll('.accordion-button'));
+  const icons = Array.from(document.querySelectorAll('.accordion-icon'));
+  const contents = Array.from(document.querySelectorAll('.accordion-content'));
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+      if (icons[i].classList.contains('rotated'))
+        icons[i].style.transform = 'rotate(0deg)';
+      else {
+        icons[i].style.transform = 'rotate(180deg)';
+      }
+      icons[i].classList.toggle('rotated');
+      contents[i].classList.toggle('accordion--active');
+    });
+  }
+})();
